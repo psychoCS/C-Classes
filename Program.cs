a648fea9e352284d3cfb0ce012bc0206d3d121a5
psychoCS <58235902+psychoCS@users.noreply.github.com>
2020-03-17 12:13:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week8
+{
+    class Program
+    {
+
+      // Write a program in C# Sharp to create a user define function. 
+      //Expected Output :
+      //Welcome Friends!
+      //Have a nice day!
+
+
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        static void Hello()
+
+        {
+            Console.WriteLine("Hello Friends!");
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
