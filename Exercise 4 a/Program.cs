ccb4bb65a3f877ecebfecba6b3ea3928ddf66b85
psychoCS <58235902+psychoCS@users.noreply.github.com>
2020-03-27 10:34:04 -0400

    Add files via upload
    Pattern Testing

---
+++ Exercise 4 a/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exercise_4_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4.Write a program to create the following figures:
+            a.
+            
+                 *
+                ***
+               *****
+              *******
+             *********
+            ***********
+             *********
+              *******
+               *****
+                ***
+                 *
+            */
+
+            Int32 Space = 5, Star = 1;
+            bool build = true;
+
+            while ((Space >= 0) && (Space < 6))
+            {
+                
+                for (int i = 0; i < Space; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int i = 0; i < Star; i++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+                if (Space == 0)
+                {
+                    build = false;
+                }
+
+                if (build == true)
+                {
+                    Space -= 1;
+                    Star += 2;
+                }
+
+                else { Space += 1;
+                    Star -= 2;
+                }
+            }
+            
+
+        }
+    }
+}
+
---
+++ loopingpractice/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace loopingpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1.	Write a program to create the following figures:
+            a.	
+            -----*
+            ----***
+            ---*****
+            --*******
+            -*********
+            ***********
+            * 
+            */
+
+            double Star = 1;
+
+
+            if (Star == 1)
+            {
+                Console.WriteLine("-----*");
+                Star = Star + 1;
+
+                if (Star == 2) ;
+                {
+                    Console.WriteLine("----***");
+                    Star = Star + 1;
+
+                    if (Star == 3) ;
+                    {
+                        Console.WriteLine("---*****");
+                        Star = Star + 1;
+
+                        if (Star == 4) ;
+                        {
+                            Console.WriteLine("--*******");
+                            Star = Star + 1;
+
+                            if (Star == 5) ;
+                            {
+                                Console.WriteLine("-*********");
+                                Star = Star + 1;
+
+                                if (Star == 6) ;
+                                {
+                                    Console.WriteLine("***********");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Int32 num = 6, diff = 1;
+
+            do
+            {
+
+                for (int i = 0; i < (num - diff); i++)
+                {
+                    Console.Write("-");
+                }
+
+                for (int i = 0; i < diff; i++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+                num += 1;
+                diff += 2;
+            } while (num < 12);
+
+
+        }
+    }
+}
+            
+            
+
+            
+        
+    
+
+
